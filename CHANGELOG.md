02ee18aec5b41df5095a9cdf2e0e8bbbd1d7988b
Andre Lehmann <aisberg@posteo.de>
2020-05-18 21:21:33 +0200

    refactor: clean up

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## [1.1.0] - 2020-05-18
+### Changed
+- Cleanup
+
 ## [1.0.2] - 2020-04-11
 ### Changed
 - Correct required Ansible version
--- test_plugins/boolean.py
+++ test_plugins/boolean.py
@@ -1,15 +1,16 @@
 class TestModule(object):
+
     def tests(self):
         return {
             'boolean': self.is_boolean,
         }
 
     def is_boolean(self, value):
-        """Tests if a value is of type boolean.
+        """Test if a value is of type boolean.
 
         Jinja2 >= 2.11 comes a built-in boolean test, but most systems will ship
-        an older version. This test function will be kept, until the majority of
-        distributions ship version >= 2.11.
+        an older version. Until Ansible adopts the new Jinja2 version, this test
+        can be used as a alternative.
 
         Args:
             value: A value, that shall be type tested
